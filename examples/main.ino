0d7e1d7737886c8652a015a2f64df73ad0e5dba0
Rogelio Rup <ruizp.rogelio@me.com>
2017-01-15 18:04:15 -0600

    commit!

--- examples/main.ino
+++ examples/main.ino
@@ -1,18 +1,28 @@
 #include "../multiThread.h"
 
-
 void tickle(uint8_t b){
+  Serial.print("another 5 minutes passed!")
+}
+
+void fiveMinute(uint8_t b){
   Serial.print("another minute passed!")
 }
 
 //Initialize timer
+//mThread<int> object(function, PERPETUAL || DO_ONCE || APPLICATION_CONTROL , time_in_ms);
+
 mThread<int> timer1(tickle, PERPETUAL, 60000*1);
+mThread<int> timer2(fiveMinute, PERPETUAL, 60000*5);
 
 void setup(){
   Serial.begin(9600);
+  //Trigger the timer
+  timer1.start();
+  timer2.start();
 }
 
 void loop(){
-  //timer1 process
+  //process
   timer1.process();
+  timer2.process();
 }
